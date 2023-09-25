4792dc2aeee79899a7fd6148da3b8e239d5b3569
Lars <laha0006@stud.kea.dk>
2023-09-25 13:01:48 +0200

    Best commit you've seen today. Added line 7, and edited line 6.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Git init");
+        System.out.println("Git init!");
+        System.out.println("Start of something Great.");
     }
 }
